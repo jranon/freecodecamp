e2a55ee9a1a868c548cb9e08f7605cf8dbec588e
Jesse <jesse.ranon@gmail.com>
2017-05-25 01:02:46 -0700

    See README.md note 2 for updates.

--- Advanced Front End Development Projects/Build a JavaScript Calculator/README.txt
+++ Advanced Front End Development Projects/Build a JavaScript Calculator/README.txt
@@ -3,3 +3,5 @@ Codepen - http://codepen.io/jesser702/pen/LymzYe
 Challenge here - https://www.freecodecamp.com/challenges/build-a-javascript-calculator
 
 1.  Uploading what I have so far.  Laid out buttons, classed buttons, scripted number buttons to inject their value into the window, defined math functions.  Researching to figure out how to store mathematical operations in order to chain them together after equal sign ins pressed.
+
+2.  Set object and prototyped add/subtract/multiply/divide functions.  Operator buttons pushing numbers and operator button values into entries array, setting initial value when initial value for myNum is not set, then using equals button to iterate through the array using a switch case to pick up operators and perform prototype functions.  Not working so far.  Able to get the iterator to show entry value at index, index+1.  Unable to get the operations to save their affects on myNum._val, as after iteration finishes, myNum._val is still the initial value as set by the first operation button click.
--- Advanced Front End Development Projects/Build a JavaScript Calculator/js/index.js
+++ Advanced Front End Development Projects/Build a JavaScript Calculator/js/index.js
@@ -1,86 +1,99 @@
 $(document).ready(function(){
-  
+  // define variables
   var total=0;
   var entries=[];
-  if (entries.length==0) {
-    display=0;
-  } else {
-    display=entries.join('');
+  var myNum;
+  
+  //define functions
+  function MyNum(value) {
+    this._val = value;
   }
+  
+  MyNum.prototype.add = function(addn) {
+      this._val+=addn;
+    return this;
+  }
+  
+  MyNum.prototype.subtract = function(subn) {
+      this._val-=subn;
+    return this;
+  }
+  
+  MyNum.prototype.divide = function(divn) {
+    this._val/=divn;
+    return this;
+  }
+  
+  MyNum.prototype.multiply = function(multn) {
+    this._val*=multn;
+    return this;
+  }
+  
+  // page manipulation and interactions
+  // set initial window value
   $("#current").text(total);
-  
-  function myNum(value) {
-    this._val=value;
-  }
-  
-  myNum.prototype.add = function(x) {
-     return x+myNum._val;
-  }
-  
-  function subtract(x) {
-    return function(y) {
-      return x-y;
-    }
-  }
-  
-  function divide(x) {
-    return function(y) {
-      return x/y;
-    }
-  }
-  
-  function multiply(x) {
-    return function(y) {
-      return x*y;
-    }
-  }
-  
+  // clear buttons
   $(".cbutton").click(function(){
+    
+    if ($(this).attr("value")==="ac") {
+      entries = [];
+      alert(entries);
+    } else if ($(this).attr("value")==="ce"&&$("#current").text()==0) {
+      entries.pop();
+      alert(entries);
+    }
     $("#current").text(0);
   });
-  
+  // number buttons
   $(".nbutton").click(function(){
     var value = $(this).attr("value");
     var current=$("#current").text();
-    
     if (current==0) {
       $("#current").text(value);
     } else {
       $("#current").text(current+''+value);
     } 
   });  
-  
+  // operation buttons
   $(".obutton").click(function(){
     var currval = parseFloat($("#current").text());
     $("#current").text(0);
     var operation = $(this).attr("value");
-    switch(operation) {
-      case "plus":
-        answer=add(total)(currval);
-        total+=answer;
-        $("#current").text(0);
-        break;
-      case "subtract":
-        answer=add(total)(currval);
-        total+=answer;
-        $("#current").text(0);
-        break;
-      case "divide":
-        if (answer==0) {
-          
-        } else {
-          answer=divide(total)(currval);
-          total+=answer;
-          $("#current").text(0);
-        }
-        break;
-      case "multiply":
-        answer=add(total)(currval);
-        total+=answer;
-        $("#current").text(0);
-        break;
+    entries.push(currval);
+    entries.push(operation);
+    if (myNum===undefined&&currval!=0) {
+      myNum=new MyNum(currval);
     }
-    
   });
-  
+  // equals button
+  $(".ebutton").click(function(){
+    var i=1;
+    if (typeof entries[entries.length-1]!=="number") {
+      entries.pop();
+    }
+    while (i<entries.length) {
+      switch (entries[i]) {
+        case "plus":
+          myNum.add(entries[i+1]);
+          i++;
+          break;
+        case "subtract":
+          myNum.subtract(entries[i+1]);
+          i++;
+          break;
+        case "divide":
+          myNum.divide(entries[i+1]);
+          i++;
+          break;
+        case "multiply":
+          myNum.divide(entries[i+1]);
+          i++;
+          break;
+        default:
+          i++;
+          break;
+      }
+    } 
+    console.log(myNum._val);
+  });
 });
